423d8db7f1efe728501964db353ab9ecf59adc89
Boato Andrea <163646394+aandrea-boatoo@users.noreply.github.com>
2024-11-18 17:50:47 +0100

    complete work

--- script/script.js
+++ script/script.js
@@ -1,1 +1,26 @@
 "use strict";
+
+
+const extraction = document.getElementById("emailContainer");
+const generatorButton = document.getElementById('generator')
+
+generatorButton.addEventListener('click', () => {
+    buttonMove();
+    extraction.innerHTML = " ";
+    for (let i = 0; i < 10; i++) {
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => {
+                const email = document.createElement('p');
+                email.classList.add("paragraph",)
+                email.innerHTML = response.data.response;
+                extraction.appendChild(email);
+            })
+    }
+});
+
+
+function buttonMove() {
+    generatorButton.classList.add("newPosition");
+    generatorButton.classList.remove("firstPosition");
+
+}
